665c44a51246581c021cad71295eafd1c8a28cce
Petteri <petteri.malkavaara1@gmail.com>
2021-06-30 18:46:04 +0300

    Ignore NPCs and holograms
    Fix #2

--- src/main/java/me/petterim1/clearlag/Main.java
+++ src/main/java/me/petterim1/clearlag/Main.java
@@ -1,23 +1,18 @@
-package idk.plugin.clearlag;
+package me.petterim1.clearlag;
 
 import cn.nukkit.Server;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityHuman;
-import cn.nukkit.event.Listener;
 import cn.nukkit.level.Level;
 import cn.nukkit.plugin.PluginBase;
 
-public class Main extends PluginBase implements Listener {
+public class Main extends PluginBase {
 
     @Override
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("clearlag")) {
+        if (cmd.getName().equals("clearlag")) {
             if (!sender.hasPermission("clearlag.run")) {
                 sender.sendMessage("\u00A7cYou don't have permission to use this command");
                 return true;
@@ -25,7 +20,7 @@ public class Main extends PluginBase implements Listener {
 
             for (Level level : Server.getInstance().getLevels().values()) {
                 for (Entity entity : level.getEntities()) {
-                    if (!(entity instanceof EntityHuman)) {
+                    if (!(entity instanceof EntityHuman) && (entity.namedTag == null || (!entity.namedTag.contains("npc") && !entity.namedTag.contains("hologramId")))) {
                         entity.close();
                     }
                 }
@@ -38,7 +33,6 @@ public class Main extends PluginBase implements Listener {
             System.gc();
 
             sender.sendMessage("\u00A73[ClearLag] \u00A7aLag cleared!");
-
             return true;
         }
         return false;
